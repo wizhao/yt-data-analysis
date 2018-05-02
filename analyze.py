8982ea91071487b16a3f58c2046aae39bd3e559a
sarakkal107 <sidhu.s.arakkal@gmail.com>
2018-05-02 14:16:44 -0400

    Sort Channels in Trending
    Analyze the channel with the highest frequency on the trending list.

--- analyze.py
+++ analyze.py
@@ -1,13 +1,25 @@
-import os
-import seaborn as sb
-import matplotlib as plt
-import pandas as pd
+import analyze as vz
+from collections import defaultdict as dd
 
-class analyze:
-    
-    def __init__(self, dataFile):
-        self.df = pd.read_csv(os.getcwd() + '\\Data\\' + dataFile, index_col=0)
+class country:
+     
+    def __init__(self, fileName):
+        self.data = vz.analyze(fileName)
+       
+    def channel_sort(self):
+        channels = []
+        for item in self.data.df['channel_title']:
+            channels.append(item)
+            
+        self.sortedC = [] 
+        repeat = dd(int)
         
-    def histogram(self):
-        print self.df.head() # will eventually produce a histogram
+        for item in self.channels:
+            repeat[item] += 1
+            
+        for key in repeat:
+            self.sortedC.append([key, repeat[key]])
         
+        self.sortedC.sort(key=lambda x: x[1], reverse=True)
+        
+        return self.sortedC
