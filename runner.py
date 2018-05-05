a81dff858bf2bc401e9c64108331f865e39259de
Sidhu Arakkal <sidhu.s.arakkal@gmail.com>
2018-05-04 23:43:55 -0400

    Finalized Scatterplot
    Finished scatterplotting.

--- runner.py
+++ runner.py
@@ -8,25 +8,33 @@ gbData = az.country('GBvideos.csv')
 
 usSort = usData.channel_sort()
 usChannel = usData.category_analysis()
+usOther = usData.likes_dislikes_comments()
 
 deSort = deData.channel_sort()
 deChannel = deData.category_analysis()
+deOther = deData.likes_dislikes_comments()
 
 caSort = caData.channel_sort()
 caChannel = caData.category_analysis()
+caOther = caData.likes_dislikes_comments()
 
 frSort = frData.channel_sort()
 frChannel = frData.category_analysis()
+frOther = frData.likes_dislikes_comments()
 
 gbSort = gbData.channel_sort()
 gbChannel = gbData.category_analysis()
+gbOther = gbData.likes_dislikes_comments()
 
-usData.makePieChart()
-usData.makeBarGraph()
+#usData.makePieChart()
+#usData.makeBarGraph()
+fullData = [usData.ScatterPlotData(), caData.ScatterPlotData(), 
+            deData.ScatterPlotData(), frData.ScatterPlotData(),
+            gbData.ScatterPlotData()]
 
+usData.makeScatterPlot(fullData)
 
-
-
+'''
 print usChannel
 print "\n"
 print caChannel
@@ -36,3 +44,4 @@ print "\n"
 print frChannel
 print "\n"
 print gbChannel
+'''
--- visualize.py
+++ visualize.py
@@ -29,7 +29,7 @@ class visualize:
         tips = sns.load_dataset("tips")
         ax = sns.barplot(x="day", y="total_bill", data=tips)
         ax.show()
-        
+    
     def piechart(self, dataSet):
         labels = []
         sizes = []
@@ -46,4 +46,63 @@ class visualize:
                 shadow=True, startangle=90)
         ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
         
+        plt.show()
+    
+    def scatterplot(self, dataSet):
+        i=0
+        while i < len(dataSet):                
+            x=[]
+            y=[]
+            j=0
+            while j < len(dataSet[i]):
+                x.append(int(dataSet[i][j][0]))
+                y.append(int(dataSet[i][j][1]))
+                j += 1
+            
+            if i == 0:
+                plt.subplot(232)
+                plt.scatter(x, y, color="b", s=40)
+                plt.xlabel('Likes')
+                plt.ylabel('Dislikes')
+                plt.title('US Dislikes vs Likes')
+                plt.subplot(231)
+                plt.scatter(x, y, label="United States", color="b", s=40)
+            elif i == 1:
+                plt.subplot(233)
+                plt.scatter(x, y, color="g", s=40)
+                plt.xlabel('Likes')
+                plt.ylabel('Dislikes')
+                plt.title('CA Dislikes vs Likes')
+                plt.subplot(231)
+                plt.scatter(x, y, label="Canada", color="g", s=40)
+            elif i == 2:
+                plt.subplot(234)
+                plt.scatter(x, y, color="k", s=40)
+                plt.xlabel('Likes')
+                plt.ylabel('Dislikes')
+                plt.title('GE Dislikes vs Likes')
+                plt.subplot(231)
+                plt.scatter(x, y, label="Germany", color="k", s=40)
+            elif i == 3:
+                plt.subplot(235)
+                plt.scatter(x, y, color="r", s=40)
+                plt.xlabel('Likes')
+                plt.ylabel('Dislikes')
+                plt.title('FR Dislikes vs Likes')
+                plt.subplot(231)
+                plt.scatter(x, y, label="France", color="r", s=40)
+            else:
+                plt.subplot(236)
+                plt.scatter(x, y, color="y", s=40)
+                plt.xlabel('Likes')
+                plt.ylabel('Dislikes')
+                plt.title('GB Dislikes vs Likes')
+                plt.subplot(231)
+                plt.scatter(x, y, label="Britain", color="y", s=40)
+            i += 1
+        
+        plt.legend(loc="upper right")
+        plt.xlabel('Likes')
+        plt.ylabel('Dislikes')
+        plt.title('Countries Dislikes vs Likes')
         plt.show()
