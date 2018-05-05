6322e0939aa010da1ead2fdd3478c5528764df45
Sidhu Arakkal <sidhu.s.arakkal@gmail.com>
2018-05-05 00:23:55 -0400

    Cleaning
    Cleaned code

--- runner.py
+++ runner.py
@@ -7,39 +7,38 @@ deData = az.country('DEvideos.csv')
 frData = az.country('FRvideos.csv')
 gbData = az.country('GBvideos.csv')
 
+
 usSort = usData.channel_sort()
-usChannel = usData.category_analysis()
-usOther = usData.likes_dislikes()
-
 deSort = deData.channel_sort()
-deChannel = deData.category_analysis()
-deOther = deData.likes_dislikes()
-
-
 caSort = caData.channel_sort()
-caChannel = caData.category_analysis()
-caOther = caData.likes_dislikes()
-
-
 frSort = frData.channel_sort()
-frChannel = frData.category_analysis()
-frOther = frData.likes_dislikes()
-
-
 gbSort = gbData.channel_sort()
-gbChannel = gbData.category_analysis()
-gbOther = gbData.likes_dislikes()
 
 barData = [usData.barGraphData(), caData.barGraphData(),
            deData.barGraphData(), frData.barGraphData(),
            gbData.barGraphData()]
+
+
+usOther = usData.likes_dislikes()
+deOther = deData.likes_dislikes()
+caOther = caData.likes_dislikes()
+frOther = frData.likes_dislikes()
+gbOther = gbData.likes_dislikes()
            
 scatterData = [usData.scatterPlotData(), caData.scatterPlotData(), 
             deData.scatterPlotData(), frData.scatterPlotData(),
             gbData.scatterPlotData()]
 
+
+pieData = [usData.category_analysis(), deData.category_analysis(),
+           caData.category_analysis(), frData.category_analysis(),
+           gbData.category_analysis()]
+           
 scatter = visualize()
-scatter.scatterplot(scatterData)                       
+scatter.scatterplot(scatterData)
+
+pie = visualize()
+pie.piechart(usChannel)                                              
 
 
 '''
