c64a6290e6f3aebd98f699c51d7eafe4853a15a0
Bill Zhao <bizhao@ctemc.org>
2018-05-04 23:48:46 -0400

    pie
    i try bar

--- visualize.py
+++ visualize.py
@@ -2,6 +2,7 @@ import os
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 class visualize:
     
@@ -13,7 +14,6 @@ class visualize:
         
         
     def bargraph(self, dataSet):
-        '''
         sns.set_style("whitegrid")
         #snsData = sns.load_dataset("tips")
         numbers = []
@@ -21,29 +21,41 @@ class visualize:
         for i in dataSet:
             channels.append(i[0])
             numbers.append(i[1])
-        ax = sns.barplot(x="Channel", y="Number of Days Trending", data=numbers, order=channels)
-        #ax = sns.barplot(data=self.df.repeat)
-        ax.show()
-        '''
-        sns.set_style("whitegrid")
-        tips = sns.load_dataset("tips")
-        ax = sns.barplot(x="day", y="total_bill", data=tips)
-        ax.show()
+        x_pos = [i for i, _ in enumerate(channels)]
+        
+        plt.bar(x_pos,numbers)
+        plt.show()
         
     def piechart(self, dataSet):
         labels = []
         sizes = []
+        total = 0
         for i in dataSet:
             labels.append(i[0])
             sizes.append(i[1])
-        sizes
+            total += i[1]
+        
+        other = 0
+        for i in range(len(sizes)):
+            if float(sizes[i])/total < 0.05:
+                del labels[i]
+                other += sizes[i]
+                del sizes[i]
+                i = i-1
+        
+        if other > 0:
+            labels.append("Other")
+            sizes.append(other)
+                
         
         def autopct_more_than_5(pct):
             return ('%1.f%%' % pct) if pct > 5 else ''
-        
+        '''
+        def autolbl_more_than_5(pct):
+            return labels[i] if pct > 5 else ''
+        '''
         fig1, ax1 = plt.subplots()
-        ax1.pie(sizes, labels=labels, autopct=autopct_more_than_5,
-                shadow=True, startangle=90)
+        ax1.pie(sizes, labels=labels, autopct=autopct_more_than_5, startangle=45,labeldistance=1.3)
         ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
         
         plt.show()
