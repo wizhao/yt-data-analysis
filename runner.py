d4bb2f554392d4287a495419a61029d8ef4e062c
Bill Zhao <bizhao@ctemc.org>
2018-05-05 02:45:45 -0400

    runner comments

--- runner.py
+++ runner.py
@@ -1,24 +1,24 @@
 import analyze as az
 from visualize import *
 
-usData = az.country('USvideos.csv')
+usData = az.country('USvideos.csv') #gather data from csv files
 caData = az.country('CAvideos.csv')
 deData = az.country('DEvideos.csv')
 frData = az.country('FRvideos.csv')
 gbData = az.country('GBvideos.csv')
 
 
-usSort = usData.channel_sort()
+usSort = usData.channel_sort() #sorts data for further processing
 deSort = deData.channel_sort()
 caSort = caData.channel_sort()
 frSort = frData.channel_sort()
 gbSort = gbData.channel_sort()
 
 barData = [usData.barGraphData(), caData.barGraphData(),
-           gbData.barGraphData()]
+           gbData.barGraphData()] #truncates and sorts data for use in bar graph
 
 
-usOther = usData.likes_dislikes()
+usOther = usData.likes_dislikes() #adds up likes and dislikes for further processing
 deOther = deData.likes_dislikes()
 caOther = caData.likes_dislikes()
 frOther = frData.likes_dislikes()
@@ -26,24 +26,24 @@ gbOther = gbData.likes_dislikes()
            
 scatterData = [usData.scatterPlotData(), caData.scatterPlotData(), 
             deData.scatterPlotData(), frData.scatterPlotData(),
-            gbData.scatterPlotData()]
+            gbData.scatterPlotData()] #organizes data for scatter plots
 
 
 pieData = [usData.category_analysis(), deData.category_analysis(),
            caData.category_analysis(), frData.category_analysis(),
-           gbData.category_analysis()]
+           gbData.category_analysis()] #analyzes data for use in pie chart
 
-bar = visualize()
+bar = visualize() #creates bar graphs
 bar.bargraph(barData)
 
-pie = visualize()                                             
+pie = visualize()  #creates pie charts
 pie.piechart(pieData)
                    
-scatter = visualize()
+scatter = visualize() #creates scatter plots
 scatter.scatterplot(scatterData)                                          
 
 
-
+#testing purposes
 '''
 print usChannel
 print "\n"
